66d4a058475a1696a70cd3f29ff3ccf10ceaa1ad
KwtsPls <constantinepls99@gmail.com>
2024-04-24 13:34:39 +0300

    Added resource files

---
+++ ViewStrategem/MVIndex/src/main/java/gr/uoa/di/interfaceAdapters/Resources.java
@@ -0,0 +1,54 @@
+package gr.uoa.di.interfaceAdapters;
+
+public class Resources {
+
+	//General resources
+	public static String fileWithDBPediaLog = "GSpan/resources/dbpedia.log-valid-select";
+	public static String fileWithDBPediaExtractedQueries = "GSpan/resources/dbpedia.paths-cycles";
+	public static String fileWithDBPediaCycles = "GSpan/resources/dbpedia-cycles";
+	public static String fileWithDBPediaPaths = "GSpan/resources/dbpedia-property-paths";
+
+	public static String fileWithLsqLog = "GSpan/resources/lsq.log-valid-select";
+	public static String fileWithLsqLogExtractedQueries = "GSpan/resources/lsq.paths-cycles";
+	public static String fileWithLsqCycles = "GSpan/resources/lsq-cycles";
+	public static String fileWithLsqPaths = "GSpan/resources/lsq-property-paths";
+
+	public static String fileWithRoadnetLog = "GSpan/resources/roadnet.log-generated";
+	public static String fileWithRoadnetLogExtractedQueries = "GSpan/resources/roadnet.paths-cycles";
+	public static String fileWithRoadnetCycles = "GSpan/resources/roadnet-cycles";
+	public static String fileWithRoadnetPaths = "GSpan/resources/roadnet-property-paths";
+
+	public static String fileWithColoursLog = "GSpan/resources/colours.log-generated";
+	public static String fileWithColoursLogExtractedQueries = "GSpan/resources/colours.paths-cycles";
+	public static String fileWithColoursCycles = "GSpan/resources/colours-cycles";
+	public static String fileWithColoursPaths = "GSpan/resources/colours-property-paths";
+
+	//Neo4j resources
+	public static String uriNeo4jMain = "";
+	public static String uriNeo4jView = "";
+
+	public static String fileWithCsvViews = "";
+	public static String fileWithCsvViewsLSQ = "";
+	public static String fileWithCsvViewsRoadnet = "";
+	public static String fileWithCsvViewsColours = "";
+
+	public static String usernameNeo4jDBPediaMain = "";
+	public static String passwordNeo4jDBPediaMain = "";
+	public static String usernameNeo4jDBPediaView = "";
+	public static String passwordNeo4jDBPediaView = "";
+
+	public static String usernameNeo4jLSQMain = "";
+	public static String passwordNeo4jLSQMain = "";
+	public static String usernameNeo4jLSQView = "";
+	public static String passwordNeo4jLSQView = "";
+
+	public static String usernameNeo4jROADNETMain = "";
+	public static String passwordNeo4jROADNETMain = "";
+	public static String usernameNeo4jROADNETView = "";
+	public static String passwordNeo4jROADNETView = "";
+
+	public static String usernameNeo4jCOLOURSMain = "";
+	public static String passwordNeo4jCOLOURSMain = "";
+	public static String usernameNeo4jCOLOURSView = "";
+	public static String passwordNeo4jCOLOURSView = "";
+}
